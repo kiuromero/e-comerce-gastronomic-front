b08dbf0906b40ce1ffd2f19a7725a385fb33bcd5
Kelmer <kelmerdromero@hotmail.com>
2021-03-15 22:57:48 -0500

    se crean primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './home/nav-bar/nav-bar.component';
+import { SliderComponent } from './home/slider/slider.component';
+import { CardProductComponent } from './home/card-product/card-product.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SliderComponent,
+    CardProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
