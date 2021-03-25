441e75ab92e3950b2be0ca75958b3fd639b91a00
jonathan <jonatanvega90@hotmail.com>
2021-03-24 20:22:59 -0500

    se agregan secciones del home y estilos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SliderComponent } from './slider/slider.component';
 import { CardProductComponent } from './card-product/card-product.component';
+import { SectionComponent } from './section/section.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainHomeComponent } from './main-home/main-home.component';
 import {HomeRoutingModule} from './home-routing.module'
@@ -13,6 +14,7 @@ import { LoginComponent } from '../auth/login/login.component';
     CardProductComponent,
     FooterComponent,
     MainHomeComponent,
+    SectionComponent,
 
     //Angular Material    
   ],
